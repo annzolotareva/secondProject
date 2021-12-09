adc1895dae5009ae71ae4340daf56c5efc8baa84
Anna Zolotareva <anazolotarewa@yandex.ru>
2021-12-09 16:19:28 +0400

    Выполнила задания 10 урока

--- js/script.js
+++ js/script.js
@@ -1,26 +1,31 @@
+"use strict";
 const books = document.querySelectorAll(".book");
-const advertising = document.querySelector(".adv");
-const titleOfBooks = document.querySelectorAll("a");
+const titlesOfBooks = document.querySelectorAll("a");
 const shapters = document.querySelectorAll("ul");
-const shapter2 = shapters[0].querySelectorAll("li");
-const shapter5 = shapters[5].querySelectorAll("li");
-const shapter6 = shapters[2].querySelectorAll("li");
-
-shapter2[3].after(shapter2[6], shapter2[8]);
-shapter2[9].after(shapter2[2]);
-
-shapter5[1].after(shapter5[9]);
-shapter5[4].after(shapter5[2]);
-shapter5[8].before(shapter5[5]);
-
+const shaptersOfBook2 = shapters[0].querySelectorAll("li");
+const shaptersOfBook5 = shapters[5].querySelectorAll("li");
+const shaptersOfBook6 = shapters[2].querySelectorAll("li");
 const chapter8OfBook6 = document.createElement("li");
-chapter8OfBook6.textContent = "Глава 8: За пределами ES6";
-shapter6[8].append(chapter8OfBook6);
 
-titleOfBooks[4].text = "Книга 3. this и Прототипы Объектов";
-
-advertising.remove();
+const advertising = document.querySelector(".adv");
+const body = document.querySelector("body");
 
 books[5].after(books[2]);
 books[1].after(books[0]);
 books[4].after(books[3]);
+
+body.style.backgroundImage = "url(./image/you-dont-know-js.jpg)";
+
+titlesOfBooks[4].text = "Книга 3. this и Прототипы Объектов";
+
+advertising.remove();
+
+shaptersOfBook2[3].after(shaptersOfBook2[6], shaptersOfBook2[8]);
+shaptersOfBook2[9].after(shaptersOfBook2[2]);
+
+shaptersOfBook5[1].after(shaptersOfBook5[9]);
+shaptersOfBook5[4].after(shaptersOfBook5[2]);
+shaptersOfBook5[8].before(shaptersOfBook5[5]);
+
+chapter8OfBook6.textContent = "Глава 8: За пределами ES6";
+shaptersOfBook6[8].append(chapter8OfBook6);
